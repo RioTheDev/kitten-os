f2f5be7237bd2a068a65d8ed805b1c33238b7e84
RioTheDev <87861408+RioTheDev@users.noreply.github.com>
2024-02-18 02:46:36 +0400

    add logo and meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,16 @@
 	});
 </script>
 
+<svelte:head>
+	<title>KittenOS</title>
+	<meta name="keywords" content="KittenOS, terminal, cmd" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<meta
+		name="description"
+		content="Play around with KittenOS where cats help you manage your files!"
+	/>
+</svelte:head>
 <main
 	style:color
 	class="max-w-[100vw] w-screen overflow-clip font-consolas h-screen max-h-screen bg-black font-light text-xs md:text-xl p-2 py-5"
